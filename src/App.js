8487281a9243393d261252f43c2a51bae03ebef2
Joey Lee <joeylee@comp.nus.edu>
2023-10-05 22:15:55 +0800

    Add skeleton, routing, removed readme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Profile from './pages/Profile/Profile';
+import NoPage from './pages/NoPage/NoPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Profile />}/>
+        <Route path="home" element={<Home />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/AddBen/AddBen.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AddBen() {
+  return (
+    <div>AddBen</div>
+  )
+}
